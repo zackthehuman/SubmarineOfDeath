b3f810f4d6cf09a9fd0ee6a8a30851f27b7a68f8
Timmy Sjöstedt <git@iostream.se>
2014-04-26 18:59:08 +0200

    Added event handling.

--- src/main.cpp
+++ src/main.cpp
@@ -5,4 +5,22 @@ sf::RenderWindow window;
 int main()
 {
 	window.create({800, 600}, "Beneath the Surface");
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				switch (event.key.code) {
+				case sf::Keyboard::Escape:
+					window.close();
+					break;
+				}
+				break;
+			}
+		}
+	}
 }
